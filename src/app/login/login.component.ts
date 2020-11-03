48c3f78d8c42dacbb6fc29d692c921a9418dd927
srlogs <logeshwaran.sr1@gmail.com>
2020-11-03 14:22:04 +0530

    Added layouts for login and registration

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  email : string;
+  password : string;
+  invalid : boolean = false;
   constructor() { }
+
+  authenticateData() {
+    console.log("login successfull!");
+  }
 
   ngOnInit(): void {
   }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -7,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  // username : string;
+  // email : string;
+  // password : string;
+  // phone : string;
+  submitted = false;
+  form : FormGroup;
+  returnUrl : string;
+  constructor(private formBuilder : FormBuilder) { }
+  addUserData() {
+    this.submitted = true;
 
+    if(this.form.invalid) {
+      return;
+    }
+  }
   ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      name : [' ', Validators.required],
+      phone : [' ', Validators.required],
+      email : [' ', Validators.required],
+      password : [' ', Validators.required]
+    })
+
+    this.returnUrl = '/';
+  }
+
+  get f() {
+    return this.form.controls;
   }
 
 }
